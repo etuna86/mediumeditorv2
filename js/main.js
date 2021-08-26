20efa5cf64498063ba136618e99204bf86338206
erol tuna <eroltuna86@gmail.com>
2021-08-26 10:47:51 +0300

    kodlara yorum eklendi

--- js/main.js
+++ js/main.js
@@ -6,49 +6,45 @@
     keyboard: false
   })
 
- /** modal  oluşturuluyor comment için **/
+ /** text  adında bir alan localstorage de oluşturuluyor var ise text değişkenine atanıyor (bu kısmları json datası olarak düşünebiliriz bu kısım html olarak gelmeli) **/
   if(!localStorage.getItem('text')){
     text="<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> ";
   
   }else{
     text=localStorage.getItem('text');
 
+    /*** document.ready fonksiyonu render işlemini gerçekleştiriyor ***/
     document.addEventListener("DOMContentLoaded", function(e) {
-   // var tag = document.createElement("div");
+
+    /***   tagları çekiyoruz ***/
    var tag = document.getElementById("editabletext");
-   //var tags= [];
+
+    /***  comment adlı tagları çekiyoruz ***/
    var tags=tag.getElementsByTagName("comment");
 
-   var arr = Array.from(tags); 
-   //tags = [...tag.getElementsByTagName("comment")];
-   console.warn("tags12312321312; ", tags);
-   
-   console.warn("tags12312321312; ", tag.getElementsByTagName("comment").length);
-   //tags=Array.from(tag.getElementsByTagName("comment"));
-
-      /*** yorum yapılan cümleler burada yeniden oluşturuluyor */
-   for (let i = 0; i < tags.length; i++) {
-    console.warn("tags; ", tags[i]);
-    tag.getElementsByTagName("comment")[i].ondblclick = function(ev) { seeComment(ev);}
-
-      var attr = document.createAttribute("data-comment234");       // Create a "class" attribute
-      attr.value = "comment2123";   
-
-      console.warn("attr; ",);
-      tag.getElementsByTagName("comment")[i].setAttributeNode(attr);
-
-    } 
+    /*** yorum yapılan cümleler burada yeniden oluşturuluyor */
+    /***  sayfa yenilendiğinde js event'leri kaybolduğu için  fonksiyonu dom'da tekrar oluşturmamız gerekiyor  ***/
+    for (let i = 0; i < tags.length; i++) {
+        console.warn("tags; ", tags[i]);
+        /*** ilgili elment için olusturulan yorum "penceresi acma" ( seeComment() ) fonksiyonu burada tekrar oluşturuluyor ***/
+        tag.getElementsByTagName("comment")[i].ondblclick = function(ev) { seeComment(ev);}
+        //tag.getElementsByTagName("comment")[i].setAttributeNode(attr);
+      } 
     });
 
   }
 
+
+  /***  text'ten gelen veriyi dom elementine ekliyoruz ***/
   getAllText('editabletext',text);
 
+   /***  text'ten gelen veriyi dom elementine ekliyoruz ***/
   function getAllText(getElementID,text){
      var el= document.getElementById(getElementID);
      el.innerHTML=text;
   }
 
+  /*** Medium editor'e harici bir button ekliyoruz  ***/
   var HighlighterButton = MediumEditor.Extension.extend({
     name: 'highlighter',
   
@@ -56,9 +52,8 @@
       this.button = this.document.createElement('button');
       this.button.classList.add('medium-editor-action');
       var commentEl=document.createElement('comment');
-      var attr = document.createAttribute("data-comment");       // Create a "class" attribute
+      var attr = document.createAttribute("data-comment");
       attr.value = "comment1";   
-      //this.button.createAttribute('data-comment');
       commentEl.setAttributeNode(attr);
       commentEl.appendChild(document.createTextNode('Comment'));
       this.button.appendChild(commentEl);
@@ -70,6 +65,8 @@
       return this.button;
     }
   });
+
+    /*** Bu kısım Medium editor'ü çalıştırıdığımız alan  ***/
 /** medium-editor start **/
   var editor = new MediumEditor('.editable', {
     toolbar: {
@@ -151,8 +148,11 @@ function replaceSelectedText() {
       newNode.setAttributeNode(att);
       var att = document.createAttribute("data-commentid"); 
       var newCommentId=Date.now();
-      //newNode.ondblclick = function() { alert('blah'); };
+      
+
+ 
       newNode.ondblclick = function(ev) { seeComment(ev);}
+      //newNode.ondblclick = function() { alert('blah'); };
       att.value = newCommentId;  
       newNode.setAttributeNode(att);
       newNode.appendChild(content);
@@ -182,7 +182,8 @@ function replaceSelectedText() {
 }
 
 
-
+/** tıklanan elementin commentid sini alıp getCommentID  fonksiyonuna gönderiyor  **/
+//ayrıca bu fonksiyon her sayfa yenilemede  ilgili comment elementlerine yeniden atanıyor(DOM'da  yeniden oluşturulmak zorunda )
 function seeComment(e){
   console.warn("seeComment element: ",e.target);
   console.warn("seeComment element dataset: ",e.target.dataset.commentid);
@@ -195,7 +196,7 @@ function removeElement(){
 }
 
 
-
+/*** Tıklanan element içerisindeki elementlere ortak id  atanıyor ***/
 function getCommentID(getValue){
   var commentTextArea = document.getElementById('commenttextarea');
   var comments = document.getElementById('comments');
@@ -205,6 +206,7 @@ function getCommentID(getValue){
   console.warn("getcommentid: ",getValue);
 
   if(localStorage.getItem(getValue)){
+    /***yayıncı tarafından yazılan yorumların kaydedilmesi (şimdilik bir yorum yazılabiliyor :D diğeri siliniyor) ***/
     comments.innerHTML=localStorage.getItem(getValue);
   }
   else{
@@ -215,7 +217,7 @@ function getCommentID(getValue){
   console.warn("asdadad; ",commentTextArea.value);
   //localStorage.setItem(getValue,commentTextArea.value);
 
-
+ /*** bu kısım react ta daha az kod ile düzenlenebilir!!! ***/
   var att = document.createAttribute("data-comid"); 
   var att2 = document.createAttribute("data-tcomid"); 
   var att3 = document.createAttribute("data-formid"); 
